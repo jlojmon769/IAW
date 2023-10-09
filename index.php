7e2e004e4fb5ac878332cc2e251779e40e9451e2
jlojmon769 <146702331+jlojmon769@users.noreply.github.com>
2023-10-09 09:48:48 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>iawgithub</title>
+    </head>
+    <body>
+        <header>
+            <h1> hola </h1>
+        </header>
+        <?php
+          if($edad >= 18){
+              echo "si yo tengo 18 años soy adulto";
+          }
+           else {
+               echo "tengo 17 no soy adulto";
+        ?>
+    </body>
+</html>
